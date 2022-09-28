7d9eed0ce9a20ab8bd5ff6f155677968de57da1f
tatjana.schaefer <tatjana.schaefer@santismail.ch>
2022-09-28 14:49:20 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+	import FeedbackList from './componets/FeedbackList.svelte';
+	
+		let feedback = [
+	{
+		id: 1,
+		rating: 10,
+		text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat',
+	},
+	{
+		id: 2,
+		rating: 9,
+		text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat',
+	},
+	{
+		id: 3,
+		rating: 8,
+		text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat',
+	},
+	]
+</script>
+
+<main>
+	<FeedbackList {feedback} />
+</main>
